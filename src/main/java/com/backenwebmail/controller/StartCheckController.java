0fe3221fd2ae1632bd34dd7aa599e913f77cf2f8
alejandrosubero <alejandrosubero.ar@gmail.com>
2021-11-01 10:47:49 -0300

    new controller to check

--- src/main/java/com/backenwebmail/controller/StartCheckController.java
+++ src/main/java/com/backenwebmail/controller/StartCheckController.java
@@ -1,19 +1,19 @@
 package com.backenwebmail.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/")
-public class IndexController {
+public class StartCheckController {
 
-  //  @GetMapping("/")
-    public String index() {
-        return "/index.html";
+    @GetMapping("/start")
+    public String startTest() {
+        return " <h1>!!!!!!!!!!!!!!!!!Hello Mundo!!!!!!!!!!!!</h1>"+
+                "<br>" +
+                "<h2> !!!!!!!!!!!Estoy funcionando!!!!!!!!! </h2>";
     }
-
 }
